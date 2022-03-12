cedd0d1607c7affdcd05cddd1355e8ee3ca37afa
Felipe Greggio <greggiof@gmail.com>
2022-03-12 15:45:21 -0300

    Adicionados algoritmos de ordenação Selection Sort, Bubble Sort e algoritmos de busca binária

---
+++ Ordenacao/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		int[] vetor = {3,6,8,1,4,9,0};
+		
+		Sort.selectionSort(vetor);
+		
+		int[] vetor2 = {3,6,8,1,4,9,0};
+		
+		Sort.bubbleSort(vetor2);
+	}
+}
+
+
+
